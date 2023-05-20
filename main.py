38fb22e7da2e8a07141c236b98440840718ee748
Sanjay Sankaran <zyugyzarc@gmail.com>
2023-05-20 13:49:51 -0700

    update

--- main.py
+++ main.py
@@ -1,54 +1,9 @@
 from window import * 
+from nodes import *
 
 app = QApplication([])
 
 window = AppWindow()
-
-class ValueNode(Node):
-
-    def __init__(self, scene, id=""):
-
-        super().__init__(scene, "Value")
-
-        Socket(self, "Output", "#4080d0", type=OUTPUT)
-
-        self.textedit = self.addWidget(QTextEdit("HUNGUS"))
-
-    def __eval__(self):
-
-        return self.textedit.toPlainText()
-
-class ViewerNode(Node):
-
-    def __init__(self, scene, id=""):
-
-        super().__init__(scene, "Viewer")
-
-        Socket(self, "Value", "#4080d0", type=INPUT)
-
-        self.button = self.addWidget(QPushButton("HUNGUS"))
-        self.button.clicked.connect(self.eval)
-
-    def __eval__(self, value):
-
-        self.button.setText(value)
-
-
-class MathNode(Node):
-
-    def __init__(self, scene, id=""):
-
-        super().__init__(scene, "Math")
-
-        Socket(self, "Value", "#4080d0", type=INPUT)
-        Socket(self, "Value", "#4080d0", type=INPUT)
-        Socket(self, "Output", "#4080d0", type=OUTPUT)
-
-        self.textedit = self.addWidget(QTextEdit("HUNGUS"))
-
-    def __eval__(self, val1, val2):
-        op = self.textedit.toPlainText()
-        return str(eval(val1+op+val2))
 
 val2 = ValueNode(window.scene)
 val1 = ValueNode(window.scene)
